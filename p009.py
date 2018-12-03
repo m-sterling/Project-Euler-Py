e7092759016a5e15156b46fa9f96327585b7e7da
Chris Biskup <biskupchristian01@gmail.com>
2018-12-03 16:58:31 -0500

    Updated problem 9

--- p009.py
+++ p009.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     for a in range(1000):
         for b in range(a+1, 1000):
             for c in range(b+1, 1000):
-                if a < b and b < c and math.pow(a, 2) + math.pow(b, 2) == math.pow(c, 2) and a + b + c == 1000:
+                if a < b and b < c and a**2 + b**2 == c**2 and a + b + c == 1000:
                     print(a*b*c)
                     sys.exit(0)
