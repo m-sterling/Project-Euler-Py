9619916c5bf4758e2171bc07045c81b2935fc3ea
Chris Biskup <biskupchristian01@gmail.com>
2018-12-03 16:59:38 -0500

    Updated problem 9

--- p009.py
+++ p009.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
-    import math, sys
+    import sys
     for a in range(1000):
         for b in range(a+1, 1000):
             for c in range(b+1, 1000):
                 if a < b and b < c and a**2 + b**2 == c**2 and a + b + c == 1000:
                     print(a*b*c)
-                    sys.exit(0)
+                    sys.exit(0) # used instead of a long for/else chain
