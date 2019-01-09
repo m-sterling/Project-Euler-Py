ca02d16deadd0145f88da33b383786beb4ee8e5f
Chris Biskup <biskupchristian01@gmail.com>
2019-01-09 04:08:09 -0500

    Completed problem 40

--- p040.py
+++ p040.py
@@ -1,2 +1,8 @@
 if __name__ == "__main__":
-    
+    n = "." # offset indices by 1
+    i = 1
+    while len(n) - 1 < 1000000:
+        n += str(i)
+        i += 1
+        
+    print(int(n[1]) * int(n[10]) * int(n[100]) * int(n[1000]) * int(n[10000]) * int(n[100000]) * int(n[1000000]))
