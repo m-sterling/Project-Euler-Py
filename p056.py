6679faea8962a85cbc60f477cfdb2b5dbf31f815
Chris Biskup <biskupchristian01@gmail.com>
2019-03-03 19:28:38 -0500

    Completed problem 56

--- p056.py
+++ p056.py
@@ -1,2 +1,13 @@
 if __name__ == "__main__":
-    
+    def calc_sum(num):
+        s = 0
+        for i in str(num):
+            s += int(i)
+        return s
+
+    max_sum = 0
+    for a in range(100):
+        for b in range(100):
+            max_sum = max(max_sum, calc_sum(a**b))
+
+    print(max_sum)
